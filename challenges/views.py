d1e3e218928283bd6cb92ec9e44362e89434aaea
Kasra Nadri <kasra.n2018@gmail.com>
2024-12-11 10:51:40 +0330

    added  a header and a 404 page

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.http import Http404
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january" : "Eat no meat for the entire month",
@@ -47,4 +49,4 @@ def monthly_challenge(req, month):
             "month": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported</h1>")
+        raise Http404()
